6ec6078aeafd15f6380d45ab7ebd8beb86e9cb99
williamyap90 <william.yap90@gmail.com>
2021-09-13 15:58:44 +0100

    initial setup with listen server

--- dist/config/app.js
+++ dist/config/app.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+	return (mod && mod.__esModule) ? mod : { 'default': mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
-    res.send('hello');
+	res.send('hello');
 });
 exports.default = app;
--- lib/config/app.ts
+++ lib/config/app.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello');
+	res.send('hello');
 });
 
 export default app;
