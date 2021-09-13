6c420efa302fdb8f15b15ee6b1def2b01891178e
williamyap90 <william.yap90@gmail.com>
2021-09-13 16:47:31 +0100

    add connection to mongodb

--- dist/config/app.js
+++ dist/config/app.js
@@ -4,7 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
+const dbURL = process.env.DATABASE_URL || '';
+mongoose_1.default.connect(dbURL);
+const db = mongoose_1.default.connection;
+db.on('error', (err) => {
+    console.log(err);
+});
+db.once('open', () => {
+    console.log('Connected to mongoose.');
+});
 app.get('/', (req, res) => {
     res.send('hello');
 });
--- lib/config/app.ts
+++ lib/config/app.ts
@@ -1,9 +1,24 @@
 import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Application = express();
+const dbURL = process.env.DATABASE_URL || '';
+mongoose.connect(dbURL);
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.log(err);
+});
+db.once('open', () => {
+    console.log('Connected to mongoose.');
+});
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('hello');
+    res.send('hello');
 });
 
 export default app;
