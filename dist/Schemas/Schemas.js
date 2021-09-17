779fe124e990f1bfa3119a7ae633165c56801e9d
williamyap90 <william.yap90@gmail.com>
2021-09-17 15:15:09 +0100

    add structure of restraurant object from yelpApi commented for later

--- dist/Schemas/Schemas.js
+++ dist/Schemas/Schemas.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Event = exports.User = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-// import {db} from '../db/connection';
 const UserSchema = new mongoose_1.default.Schema({
     firstName: String,
     lastName: String,
@@ -15,6 +14,10 @@ const UserSchema = new mongoose_1.default.Schema({
     },
     eventHistory: [{ eventId: String }],
     password: String,
+    avatarUrl: {
+        type: String,
+        default: 'https://climatefutures.net/cccd/images/sampleImage.png',
+    },
 }, { collection: 'users', versionKey: false });
 const EventSchema = new mongoose_1.default.Schema({
     eventName: {
@@ -44,3 +47,16 @@ const EventSchema = new mongoose_1.default.Schema({
 }, { collection: 'events', versionKey: false });
 exports.User = mongoose_1.default.model('User', UserSchema);
 exports.Event = mongoose_1.default.model('Event', EventSchema);
+// Object from front end - single restaurant
+// {
+//     name:'fuel',
+//     categories:['string','string'],
+//     coordinates: {longitude:51,latitude:55},
+//     image_url: 'http://www.google.com',
+//     display_address:[address],
+//     phone_no: 05125414,
+//     rating: 5,
+//     price: '£',
+//     review_count: 51,
+//     url: 'google'
+// }
