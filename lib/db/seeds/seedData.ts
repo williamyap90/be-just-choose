d27182d70df2ebf65da51363cdae340c30797916
williamyap90 <william.yap90@gmail.com>
2021-09-17 19:54:21 +0100

    removed console log

--- lib/db/seeds/seedData.ts
+++ lib/db/seeds/seedData.ts
@@ -3,17 +3,15 @@ import mongoose from 'mongoose';
 import { dbURL } from '../connection';
 import { User, Event } from '../../Schemas/Schemas';
 
-
 export const seedDb = () => {
-
     mongoose.connect(dbURL);
-    
+
     User.collection.drop();
     Event.collection.drop();
-    
+
     User.create(usersList)
         .then((user: any) => {
-            console.log(`${user.length} users seeded`);
+            // console.log(`${user.length} users seeded`);
         })
         .catch((err: any) => {
             console.log(err);
