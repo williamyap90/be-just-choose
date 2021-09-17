695aa05cec58a86c30867804ee7f1d5221e2bc4d
williamyap90 <william.yap90@gmail.com>
2021-09-17 09:27:25 +0100

    comment out interface

--- dist/db/seeds/seed.js
+++ dist/db/seeds/seed.js
@@ -1,16 +1,22 @@
 "use strict";
-// import faker from 'faker';
 const faker = require('faker');
 // const db = require('../../app');
 // const User = require('../../Schemas/Schemas');
 // import User  from '../../Schemas/Schemas';
+// interface User {
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+//     password: string;
+//     eventHistory: []
+// }
 const usersList = [];
 for (let i = 0; i < 20; i++) {
     const firstName = faker.name.firstName();
     const lastName = faker.name.lastName();
     const email = firstName.toLowerCase() + lastName.toLowerCase() + '@test-jc.com';
     const password = faker.internet.password();
-    let user = {
+    const user = {
         firstName,
         lastName,
         email,
@@ -20,6 +26,8 @@ for (let i = 0; i < 20; i++) {
     usersList.push(user);
 }
 console.log(usersList, ' UL<<');
+module.exports = { usersList };
+// export default usersList;
 // User.insertMany(usersList).then(() => {
 //     console.log('users inserted!');
 // });
