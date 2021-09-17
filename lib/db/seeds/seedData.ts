15458a687cd0e600605ea2d4be052d11e9aac92a
JustMatty26 <mwlwebb@gmail.com>
2021-09-17 16:22:48 +0100

    export seeding as function for testing

--- lib/db/seeds/seedData.ts
+++ lib/db/seeds/seedData.ts
@@ -3,18 +3,22 @@ import mongoose from 'mongoose';
 import { dbURL } from '../connection';
 import { User, Event } from '../../Schemas/Schemas';
 
-mongoose.connect(dbURL);
 
-User.collection.drop();
-Event.collection.drop();
+export const seedDb = () => {
 
-User.create(usersList)
-    .then((user: any) => {
-        console.log(`${user.length} users seeded`);
-    })
-    .catch((err: any) => {
-        console.log(err);
-    })
-    .finally(() => {
-        mongoose.connection.close();
-    });
+    mongoose.connect(dbURL);
+    
+    User.collection.drop();
+    Event.collection.drop();
+    
+    User.create(usersList)
+        .then((user: any) => {
+            console.log(`${user.length} users seeded`);
+        })
+        .catch((err: any) => {
+            console.log(err);
+        })
+        .finally(() => {
+            mongoose.connection.close();
+        });
+};
