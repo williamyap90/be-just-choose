d34a3f5e14145d4239b3b3dac52421c8a0e7ae72
williamyap90 <william.yap90@gmail.com>
2021-09-18 17:06:34 +0100

    removed connection & disconnection for seedDB function

--- lib/db/seeds/seedData.ts
+++ lib/db/seeds/seedData.ts
@@ -1,22 +1,16 @@
 import { usersList } from './seed';
-import mongoose from 'mongoose';
-import { dbURL } from '../connection';
 import { User, Event } from '../../Schemas/Schemas';
 
 export const seedDb = () => {
-    mongoose.connect(dbURL);
-
     User.collection.drop();
-    Event.collection.drop();
+    //commented out event as doesn't exist yet
+    // Event.collection.drop();
 
-    User.create(usersList)
+    User.insertMany(usersList)
         .then((user: any) => {
             // console.log(`${user.length} users seeded`);
         })
         .catch((err: any) => {
             console.log(err);
-        })
-        .finally(() => {
-            mongoose.connection.close();
         });
 };
