4c5c1bea669ae034bd7521464398b48ef7f4a6e7
williamyap90 <william.yap90@gmail.com>
2021-09-18 18:27:39 +0100

    add function to seed local database which requires opening and closing connection

--- lib/db/seeds/seedDb.ts
+++ lib/db/seeds/seedDb.ts
@@ -1,12 +1,15 @@
-import { usersList } from './seed';
+import { usersData } from '../data/usersData';
 import { User, Event } from '../../Schemas/Schemas';
+import mongoose from 'mongoose';
+import { dbURL } from '../connection';
 
+// Seeding database for testing (no connection)
 export const seedDb = () => {
     User.collection.drop();
     //commented out event as doesn't exist yet
     // Event.collection.drop();
 
-    User.insertMany(usersList)
+    User.insertMany(usersData)
         .then((user: any) => {
             // console.log(`${user.length} users seeded`);
         })
@@ -14,3 +17,24 @@ export const seedDb = () => {
             console.log(err);
         });
 };
+
+// Seeding local database (connection required)
+const seedLocalDb = () => {
+    mongoose.connect(dbURL);
+
+    User.collection.drop();
+    //commented out event as doesn't exist yet
+    // Event.collection.drop();
+
+    User.insertMany(usersData)
+        .then((user: any) => {
+            console.log(`${user.length} users seeded`);
+        })
+        .catch((err: any) => {
+            console.log(err);
+        })
+        .finally(() => {
+            mongoose.connection.close();
+        });
+};
+seedLocalDb();
