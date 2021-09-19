5db0d8b8c10d0a1f96b5e91fb44516ab91887028
williamyap90 <william.yap90@gmail.com>
2021-09-19 16:07:00 +0100

    add check for collection before dropping/inserting

--- dist/db/seeds/seedDb.js
+++ dist/db/seeds/seedDb.js
@@ -1,26 +1,76 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.seedDb = void 0;
 const usersData_1 = require("../data/usersData");
 const eventsData_1 = require("../data/eventsData");
 const Schemas_1 = require("../../Schemas/Schemas");
+const mongoose_1 = __importDefault(require("mongoose"));
+const connection_1 = require("../connection");
 // Seeding database for testing (no connection)
 const seedDb = () => {
-    Schemas_1.User.collection.drop();
-    Schemas_1.Event.collection.drop();
-    Schemas_1.User.insertMany(usersData_1.usersData)
-        .then((user) => {
-        // console.log(`${user.length} users seeded`);
-    })
-        .catch((err) => {
-        console.log(err);
+    const connect = mongoose_1.default.createConnection(connection_1.dbURL);
+    connect.on('open', () => {
+        connect.db
+            .listCollections() //searching for all collections
+            // .listCollections({ name: 'users' }) //searching for user collection
+            .toArray((err, collection) => {
+            console.log(collection, 'CL'); //log search result
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (collection.length > 0) {
+                Schemas_1.User.collection.drop();
+                console.log('Users collection dropped');
+            }
+            Schemas_1.User.insertMany(usersData_1.usersData)
+                .then((user) => {
+                // console.log(`${user.length} users seeded`);
+            })
+                .catch((err) => {
+                console.log(err);
+            });
+        });
+        // Events check collection, drop if exists before inserting
+        connect.db
+            .listCollections({ name: 'events' })
+            .toArray((err, collectionNames) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (collectionNames.length > 0) {
+                Schemas_1.Event.collection.drop();
+                console.log('Events collection dropped');
+            }
+            Schemas_1.Event.insertMany(eventsData_1.eventsData)
+                .then((events) => {
+                console.log(`${events.length} events seeded`);
+            })
+                .catch((err) => {
+                console.log(err);
+            });
+        });
     });
-    Schemas_1.Event.insertMany(eventsData_1.eventsData)
-        .then((events) => {
-        console.log(`${events.length} events seeded`);
-    })
-        .catch((err) => {
-        console.log(err);
-    });
+    //BELOW CODE IS FINE - check for collections before dropping
+    // User.collection.drop();
+    // Event.collection.drop();
+    // User.insertMany(usersData)
+    //     .then((user) => {
+    //         // console.log(`${user.length} users seeded`);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+    // Event.insertMany(eventsData)
+    //     .then((events) => {
+    //         console.log(`${events.length} events seeded`);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
 };
 exports.seedDb = seedDb;
--- lib/db/seeds/seedDb.ts
+++ lib/db/seeds/seedDb.ts
@@ -6,22 +6,67 @@ import { dbURL } from '../connection';
 
 // Seeding database for testing (no connection)
 export const seedDb = () => {
-    User.collection.drop();
-    Event.collection.drop();
+    const connect = mongoose.createConnection(dbURL);
+    connect.on('open', () => {
+        connect.db
+            .listCollections() //searching for all collections
+            // .listCollections({ name: 'users' }) //searching for user collection
+            .toArray((err, collection) => {
+                console.log(collection, 'CL'); //log search result
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                if (collection!.length > 0) {
+                    User.collection.drop();
+                    console.log('Users collection dropped');
+                }
+                User.insertMany(usersData)
+                    .then((user) => {
+                        // console.log(`${user.length} users seeded`);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            });
 
-    User.insertMany(usersData)
-        .then((user) => {
-            // console.log(`${user.length} users seeded`);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+        // Events check collection, drop if exists before inserting
+        connect.db
+            .listCollections({ name: 'events' })
+            .toArray((err, collectionNames) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                if (collectionNames!.length > 0) {
+                    Event.collection.drop();
+                    console.log('Events collection dropped');
+                }
+                Event.insertMany(eventsData)
+                    .then((events) => {
+                        console.log(`${events.length} events seeded`);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            });
+    });
 
-    Event.insertMany(eventsData)
-        .then((events) => {
-            console.log(`${events.length} events seeded`);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    //BELOW CODE IS FINE - check for collections before dropping
+    // User.collection.drop();
+    // Event.collection.drop();
+    // User.insertMany(usersData)
+    //     .then((user) => {
+    //         // console.log(`${user.length} users seeded`);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+    // Event.insertMany(eventsData)
+    //     .then((events) => {
+    //         console.log(`${events.length} events seeded`);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
 };
