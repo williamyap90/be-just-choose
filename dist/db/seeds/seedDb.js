7e160648c5cb639b1a47bd9685e26bd090301a0e
williamyap90 <william.yap90@gmail.com>
2021-09-19 10:04:02 +0100

    add Event.insertMany to add sample data to database

--- dist/db/seeds/seedDb.js
+++ dist/db/seeds/seedDb.js
@@ -2,15 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.seedDb = void 0;
 const usersData_1 = require("../data/usersData");
+const eventsData_1 = require("../data/eventsData");
 const Schemas_1 = require("../../Schemas/Schemas");
 // Seeding database for testing (no connection)
 const seedDb = () => {
     Schemas_1.User.collection.drop();
-    //commented out event as doesn't exist yet
-    // Event.collection.drop();
+    Schemas_1.Event.collection.drop();
     Schemas_1.User.insertMany(usersData_1.usersData)
         .then((user) => {
         // console.log(`${user.length} users seeded`);
+    })
+        .catch((err) => {
+        console.log(err);
+    });
+    Schemas_1.Event.insertMany(eventsData_1.eventsData)
+        .then((events) => {
+        console.log(`${events.length} events seeded`);
     })
         .catch((err) => {
         console.log(err);
--- lib/db/seeds/seedDb.ts
+++ lib/db/seeds/seedDb.ts
@@ -1,4 +1,5 @@
 import { usersData } from '../data/usersData';
+import { eventsData } from '../data/eventsData';
 import { User, Event } from '../../Schemas/Schemas';
 import mongoose from 'mongoose';
 import { dbURL } from '../connection';
@@ -6,14 +7,21 @@ import { dbURL } from '../connection';
 // Seeding database for testing (no connection)
 export const seedDb = () => {
     User.collection.drop();
-    //commented out event as doesn't exist yet
-    // Event.collection.drop();
+    Event.collection.drop();
 
     User.insertMany(usersData)
-        .then((user: any) => {
+        .then((user) => {
             // console.log(`${user.length} users seeded`);
         })
-        .catch((err: any) => {
+        .catch((err) => {
+            console.log(err);
+        });
+
+    Event.insertMany(eventsData)
+        .then((events) => {
+            console.log(`${events.length} events seeded`);
+        })
+        .catch((err) => {
             console.log(err);
         });
 };
