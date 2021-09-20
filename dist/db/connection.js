63b03deeceb1184d50a801e94eebe64b346c28a9
williamyap90 <william.yap90@gmail.com>
2021-09-20 10:21:15 +0100

    add db user & pass env variables

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -11,7 +11,9 @@ if (!process.env.DATABASE_URL) {
     throw new Error('No database set');
 }
 else {
-    exports.dbURL = process.env.DATABASE_URL;
+    const dbUser = process.env.DB_USER;
+    const dbPass = process.env.DB_PASSWORD;
+    exports.dbURL = `mongodb+srv://${dbUser}:${dbPass}@justchoose.spif4.mongodb.net/just-choose?retryWrites=true&w=majority`;
 }
 exports.db = mongoose_1.default
     .connect(exports.dbURL)
