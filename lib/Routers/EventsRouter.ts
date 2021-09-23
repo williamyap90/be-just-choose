1a364439b8dfdd0233b6028fcb9bc0a3e432f5cc
Sgavinmills <sgavinmills@gmail.com>
2021-09-23 10:55:21 +0100

    addd get events by organiser

--- lib/Routers/EventsRouter.ts
+++ lib/Routers/EventsRouter.ts
@@ -4,11 +4,15 @@ import {
     getEventByName,
     patchEventByName,
     getEventById,
+    getEventsByOrganiser
 } from '../Controllers/EventsController';
 import express from 'express';
 const eventsRouter = express.Router();
 
 eventsRouter.route('/').get(getEvents).post(postEvent);
+eventsRouter.route('/eventsbyorganiser/:organiser').get(getEventsByOrganiser)
+
+
 
 eventsRouter.route('/:eventName').get(getEventByName).patch(patchEventByName);
 
