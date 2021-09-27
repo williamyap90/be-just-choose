1075c423511a772f7dfbfce1f6575c74f29428fc
williamyap90 <william.yap90@gmail.com>
2021-09-27 15:34:34 +0100

    remove console logs

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,6 +1,4 @@
 import express from 'express';
 export const handleRouter404: express.RequestHandler = (req, res, next) => {
-    console.log(res);
-    console.log('in error.ts');
     res.status(404).send({ message: 'Invalid path' });
 };
